a473c2639f826f40a9d0e13bec304a4266bbe47a
6310682726 <69451907+6310682726@users.noreply.github.com>
2022-09-22 22:34:35 +0700

    Update urls.py

--- as2/urls.py
+++ as2/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quota/', include('quota.urls')),
-    path('users/', include('users.urls')),
+    path('/', include('users.urls')),
 ]
